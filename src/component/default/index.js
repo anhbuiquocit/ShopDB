bf9539924e50bcafcb1503d7bebc42460931a498
anhbq <anhbuiquocit@gmail.com>
2022-01-09 23:29:51 +0700

    update api get list item

--- src/component/default/index.js
+++ src/component/default/index.js
@@ -6,24 +6,12 @@ router.get("/", (req, res) => {
   res.send("Okkk");
 });
 router.get("/list_item", async (req, res, next) => {
-  const {authorization} = req.headers
-  if(authorization && authorization.match(/^Bearer /g)){
-    res.send({
-      token: authorization.split(' ')[1],
-      message: 'OK'
-    })
-  }else{
-    res.send({
-      message: 'Authorization required'
-    })
-    next('Authorization');
-  }
-  // const sql = "select * from product";
-  // const results = await dbUtil.queryOne(sql);
-  // console.log(results);
-  // res.json({
-  //     data: results,
-  //     message: 'success'
-  // })
+  const sql = "select * from product";
+  const results = await dbUtil.queryOne(sql);
+  console.log(results);
+  res.json({
+      data: results,
+      message: 'success'
+  })
 });
 export default { path, router };
