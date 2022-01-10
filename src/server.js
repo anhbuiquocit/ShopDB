3fc5ae5c662b6ecedacd228b781f7cdc5f8b7e6e
anhbuiquocit <56730423+anhbuiquocit@users.noreply.github.com>
2022-01-10 15:28:49 +0700

    add fule cors

--- src/server.js
+++ src/server.js
@@ -3,19 +3,21 @@ import express from "express";
 // const morgan = require("morgan");
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import routers from './component/router.js'
+import routers from "./component/router.js";
+import { corsMiddleware } from "./middleware/Cors.js";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(morgan("combined"));
+app.use(corsMiddleware);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-for(const router of routers){
-  app.use(router.path, router.router)
+for (const router of routers) {
+  app.use(router.path, router.router);
 }
